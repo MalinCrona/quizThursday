fd120a541c5fb324bb81c6b82e6c75ab9966bc9b
MalinCrona <malincrona@Malins-MacBook.local>
2016-09-09 12:12:40 +0200

    Commit

--- quiz/views.py
+++ quiz/views.py
@@ -1,31 +1,32 @@
+from django.shortcuts import redirect
 from quiz.models import Quiz
 from django.shortcuts import render
 quizzes = [
 	{
 		"quiz_number": 1,
-   		"name": "Klassiska böcker",
-	   	"description": "Hur bra kan du dina klassiker?"
+		"name": "Klassiska böcker",
+		"description": "Hur bra kan du dina klassiker?"
 	},
 	{
 		"quiz_number": 2,
-   	   	"name": "Största fotbollslagen",
-	   	"description": "Kan du dina lag?"
+		"name": "Största fotbollslagen",
+		"description": "Kan du dina lag?"
 	},
 	{
 		"quiz_number": 3,
-   	    "name": "Världens mest kända hackare",
-	    "description": "Hackerhistoria är viktigt, kan du den?"	
+		"name": "Världens mest kända hackare",
+		"description": "Hackerhistoria är viktigt, kan du den?"	
 	},
 ]
 
 def startpage(request):
 	context = {
-	    "quizzes": Quiz.objects.all(),
+		"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/startpage.html", context)
 def quiz(request, quiz_number):
 	context = {
-	    	"quiz": Quiz.objects.get(quiz_number=quiz_number),
+			"quiz": Quiz.objects.get(quiz_number=quiz_number),
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/quiz.html", context)
@@ -34,19 +35,32 @@ def question(request, quiz_number, question_number):
 	questions = quiz.questions.all()
 	question = questions[int(question_number) - 1]
 	context = {
-    		"question_number": question_number,
-    		"question": question.question,
-	    	"answer1": question.answer1,
-    		"answer2": question.answer2,
-	    	"answer3": question.answer3,
-	    	"quiz": quiz,
+			"question_number": question_number,
+			"question": question.question,
+			"answer1": question.answer1,
+			"answer2": question.answer2,
+			"answer3": question.answer3,
+			"quiz": quiz,
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/question.html", context)
+def answer(request, quiz_number, question_number):
+	saved_answers = request.session.get(quiz_number, {})
+	answer = int(request.POST["answer"])
+	saved_answers[question_number] = answer
+	request.session[quiz_number] = saved_answers
+	question_number = int(question_number)
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	num_questions = quiz.questions.count()
+	if num_questions <= question_number:
+		return redirect("completed_page", quiz_number)
+	else:
+		return redirect("question_page", quiz_number, question_number + 1)
+
 def completed(request, quiz_number):
 	context = {
-	    	"correct": 12,
-	    	"total": 20,
+			"correct": 12,
+			"total": 20,
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/completed.html", context)
